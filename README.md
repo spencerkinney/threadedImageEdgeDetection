367d700b5a3401a2feae7ed4dc4c6d451524ea9e
Spencer Kinney <68272505+spencerkinney@users.noreply.github.com>
2022-04-25 00:01:30 +0000

    code

---
+++ README.md
@@ -0,0 +1,11 @@
+## Multithreaded Edge Detection
+This is a program that utilizes OpenMP in order to read an image and output a resultant grayscale image that highlights the edges of the image.
+
+This program uses standard c library functions to takes a BMP image and also produce the result as in the BMP file format. Any standard image format can be converted to the BMP format to use in this program.
+
+### Usage:
+***make*** to compile the program
+
+***./edge_detection.c <input_file> <output_file> <n_threads>*** to run the program
+
+Toggle the ***VERBOSE*** flag to 1 to output execution times to the console else toggle to 0
--- edge_detection.c
+++ edge_detection.c
@@ -12,11 +12,14 @@ typedef struct {
   u_int8_t blue;
 } Pixel;
 
-void convert_grayscale(int width, int height, Pixel image[height][width]); //completed
+void convert_grayscale(int width, int height,
+                       Pixel image[height][width]); // completed
 
-void apply_gaussian_blur(int width, int height, Pixel image[height][width]); //completed
+void apply_gaussian_blur(int width, int height,
+                         Pixel image[height][width]); // completed
 
-void apply_sobel_filters(int width, int height, Pixel image[height][width]); //completed
+void apply_sobel_filters(int width, int height,
+                         Pixel image[height][width]); // completed
 
 void apply_non_max_supression(int width, int height,
                               Pixel image[height][width]);
@@ -71,29 +74,36 @@ int main(int argc, char *argv[]) {
     fseek(in, padding, SEEK_CUR);
   }
 
-  double start,elapsed;
+  printf("Using %d threads\n", num_threads);
+  double start, elapsed;
   double total = 0.0;
   // Convert image to grayscale
   start = omp_get_wtime();
   convert_grayscale(width, height, image);
   elapsed = omp_get_wtime() - start;
   total += elapsed;
-  if (VERBOSE) {printf("CONVERT_TO_GRAYSCALE EXEC TIME: %fs\n", elapsed);}
-  
+  if (VERBOSE) {
+    printf("CONVERT_TO_GRAYSCALE EXEC TIME: %fs\n", elapsed);
+  }
+
   // Gaussian Blur
-  
+
   start = omp_get_wtime();
   apply_gaussian_blur(width, height, image);
   elapsed = omp_get_wtime() - start;
   total += elapsed;
-  if (VERBOSE) {printf("APPLY_GAUSSIAN_BLUR EXEC TIME: %fs\n", elapsed);}
+  if (VERBOSE) {
+    printf("APPLY_GAUSSIAN_BLUR EXEC TIME: %fs\n", elapsed);
+  }
 
   // Gradient Calculation
   start = omp_get_wtime();
   apply_sobel_filters(width, height, image);
   elapsed = omp_get_wtime() - start;
   total += elapsed;
-  if (VERBOSE) {printf("APPLY_SOBEL_FILTER EXEC TIME: %fs\n", elapsed);}
+  if (VERBOSE) {
+    printf("APPLY_SOBEL_FILTER EXEC TIME: %fs\n", elapsed);
+  }
 
   printf("TOTAL EXEC TIME: %fs\n", total);
 
@@ -114,8 +124,10 @@ int main(int argc, char *argv[]) {
 }
 
 void convert_grayscale(int width, int height, Pixel image[height][width]) {
-  for (int i = 0; i < height; i++) {
-    for (int j = 0; j < width; j++) {
+  int i, j;
+#pragma omp parallel for private(i, j) shared(image)
+  for (i = 0; i < height; i++) {
+    for (j = 0; j < width; j++) {
       int x = (image[i][j].red + image[i][j].green + image[i][j].blue) / 3;
       image[i][j].red = round(x);
       image[i][j].green = round(x);
@@ -125,101 +137,103 @@ void convert_grayscale(int width, int height, Pixel image[height][width]) {
 }
 
 void apply_gaussian_blur(int width, int height, Pixel image[height][width]) {
-    Pixel temp_image[height][width];
-    float avg = 0;
-    float rgb[3] = {0, 0, 0};
-
-    for (int i = 0; i < height; i++) {
-        for ( int j = 0; j < width; j++) {
-          //#pragma
-            for ( int k = -1; k < 2; k++) {
-                for ( int l = -1; l < 2; l++) {
-                    if (i + k >= 0 && j + l >= 0 && i + k < height && j + l < width) {
-                        rgb[0] += image[i + k][j + l].red;
-                        rgb[1] += image[i + k][j + l].green;
-                        rgb[2] += image[i + k][j + l].blue;
-                        avg++;
-                    }
-                }
-            }
-
-            // Update image values in temp image array
-            temp_image[i][j].red = round(rgb[0] / avg);
-            temp_image[i][j].blue = round(rgb[2] / avg);
-            temp_image[i][j].green = round(rgb[1] / avg);
-            
-            // Reset values
-            rgb[0] = 0; 
-            rgb[1] = 0; 
-            rgb[2] = 0;
-            avg = 0; 
+  Pixel temp_image[height][width];
+  float avg = 0;
+  float rgb[3] = {0, 0, 0};
+  int i, j, k, l;
+#pragma omp parallel for private(i, j, k, l, avg) shared(image) reduction(+ : rgb)
+  for (i = 0; i < height; i++) {
+    for (j = 0; j < width; j++) {
+      //#pragma
+      for (k = -1; k < 2; k++) {
+        for (l = -1; l < 2; l++) {
+          if (i + k >= 0 && j + l >= 0 && i + k < height && j + l < width) {
+            rgb[0] += image[i + k][j + l].red;
+            rgb[1] += image[i + k][j + l].green;
+            rgb[2] += image[i + k][j + l].blue;
+            avg++;
+          }
         }
-    }
+      }
 
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            image[i][j] = temp_image[i][j];
-        }
+      // Update image values in temp image array
+      temp_image[i][j].red = round(rgb[0] / avg);
+      temp_image[i][j].blue = round(rgb[2] / avg);
+      temp_image[i][j].green = round(rgb[1] / avg);
+
+      // Reset values
+      rgb[0] = 0;
+      rgb[1] = 0;
+      rgb[2] = 0;
+      avg = 0;
     }
+  }
+
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      image[i][j] = temp_image[i][j];
+    }
+  }
 }
 
 void apply_sobel_filters(int width, int height, Pixel image[height][width]) {
 
-    Pixel updated[height][width];
-    int color_coords[6];
-    int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
-    int Gy[3][3] = {{1, 2, 1}, {0, 0, 0}, {-1, -2, -1}};
+  Pixel updated[height][width];
+  int color_coords[6];
+  int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+  int Gy[3][3] = {{1, 2, 1}, {0, 0, 0}, {-1, -2, -1}};
+  int i, j, k, l;
+  
+#pragma omp parallel for private(i,j,k,l) shared(image) reduction(+:color_coords)
 
+  for (i = 0; i < height; i++) {
+    for (j = 0; j < width; j++) {
+      for (k = -1; k < 2; k++) {
+        for (l = -1; l < 2; l++) {
+          if (i + k >= 0 && j + l >= 0 && i + k < height && j + l < width) {
+            color_coords[0] += image[i + k][j + l].red * Gx[k + 1][l + 1];
+            color_coords[4] += image[i + k][j + l].blue * Gx[k + 1][l + 1];
+            color_coords[2] += image[i + k][j + l].green * Gx[k + 1][l + 1];
 
-    for (int i=0; i<height; i++) {
-        for (int j=0; j<width; j++) {
-            for ( int k = -1; k < 2; k++) {
-                for ( int l = -1; l < 2; l++) {
-                    if (i + k >= 0 && j + l >= 0 && i + k < height && j + l < width) {
-                        color_coords[0] += image[i + k][j + l].red * Gx[k + 1][l + 1];
-                        color_coords[4] += image[i + k][j + l].blue * Gx[k + 1][l + 1];
-                        color_coords[2] += image[i + k][j + l].green * Gx[k + 1][l + 1];
-
-                        color_coords[1] += image[i + k][j + l].red * Gy[k + 1][l + 1];
-                        color_coords[5] += image[i + k][j + l].blue * Gy[k + 1][l + 1];
-                        color_coords[3] += image[i + k][j + l].green * Gy[k + 1][l + 1];
-                    }
-                }
-            }
-            int a = round(sqrt((pow(color_coords[0], 2) + pow(color_coords[1], 2))));
-            int b = round(sqrt((pow(color_coords[4], 2) + pow(color_coords[5], 2))));
-            int c = round(sqrt((pow(color_coords[2], 2) + pow(color_coords[3], 2))));
-
-            for (int idx = 0; idx < 6; idx++) {
-              color_coords[idx] = 0;
-            }
-
-            if (a > 255) {
-                a = 255;
-            }
-            if (b > 255) {
-                b = 255;
-            }
-            if (c > 255) {
-                c = 255;
-            }
-
-            updated[i][j].red = a;
-            updated[i][j].blue = b;
-            updated[i][j].green = c;
+            color_coords[1] += image[i + k][j + l].red * Gy[k + 1][l + 1];
+            color_coords[5] += image[i + k][j + l].blue * Gy[k + 1][l + 1];
+            color_coords[3] += image[i + k][j + l].green * Gy[k + 1][l + 1];
+          }
         }
+      }
+      int a = round(sqrt((pow(color_coords[0], 2) + pow(color_coords[1], 2))));
+      int b = round(sqrt((pow(color_coords[4], 2) + pow(color_coords[5], 2))));
+      int c = round(sqrt((pow(color_coords[2], 2) + pow(color_coords[3], 2))));
+
+      for (int idx = 0; idx < 6; idx++) {
+        color_coords[idx] = 0;
+      }
+
+      if (a > 255) {
+        a = 255;
+      }
+      if (b > 255) {
+        b = 255;
+      }
+      if (c > 255) {
+        c = 255;
+      }
+
+      updated[i][j].red = a;
+      updated[i][j].blue = b;
+      updated[i][j].green = c;
     }
-    for (int a = 0; a < height; a++)
-    {
-        for (int b = 0; b < width; b++)
-        {
-            image[a][b] = updated[a][b];
-        }
+  }
+  for (int a = 0; a < height; a++) {
+    for (int b = 0; b < width; b++) {
+      image[a][b] = updated[a][b];
     }
-    return;
+  }
+  return;
 }
 
-void apply_non_max_supression(int width, int height, Pixel image[height][width]) {
+void apply_non_max_supression(int width, int height,
+                              Pixel image[height][width]) {
 
   int Z[width][height];
 }
